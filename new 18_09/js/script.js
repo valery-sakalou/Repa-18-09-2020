260285fe93546d4e3bf077f5fc6f70a5a7860388
Valery Sakalou <mimino730@tut.by>
2020-09-23 22:46:48 +0300

    commit 23 09

--- new 18_09/js/script.js
+++ new 18_09/js/script.js
@@ -1,2 +1,26 @@
-console.log(5+'5');
-console.log(www);
+
+// const numberOfFilms= +prompt('Сколько фильмов вы посмотрели?', );
+// //console.log(personalMovieDB);
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genre: {},
+//     privat: false
+// };
+
+// const lastFilm_0 = prompt('Последний просмотренный фильм?',);
+// const ball_0 = prompt('На сколько оценили его?', );
+// const lastFilm_1 = prompt('Последний просмотренный фильм?',);
+// const ball_1 = prompt('На сколько оценили его?', );
+
+
+// personalMovieDB.movies[lastFilm_0]=ball_0;
+// personalMovieDB.movies[lastFilm_1]=ball_1;
+
+//console.log(personalMovieDB);
+
+const num=50;
+
+(num==50) ? console.log('Ok!') : console.log('Error');
